892a51ac1fe5ed79e548a41574d11f5e70517f20
tontheonelove <tonlove5233@gmail.com>
2019-08-30 15:14:03 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+    $ch = curl_init("https://bx.in.th/api/"); // add your url which contains json file
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $content = curl_exec($ch);
+    curl_close($ch);
+    $json = json_decode($content, true);
+    //print_R($json);
+    $count=count($json);
+    echo'<table style="width:100%"><th>เหรียญ</th><th>ราคาล่าสุด  (บาท)</th>';
+    for($i=0;$i<$count;$i++)
+    {
+      echo'<tr><td>'.$json[$i]['secondary_currency'].'</td><td>'.$json[$i]['last_price'].'</td></tr>';
+    }
+    echo'</table>';
+?>
